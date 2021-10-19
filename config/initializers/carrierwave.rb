9cdeb60e971e6def6f8065fd883802b1e703bb36
”HIDEKIHIDEKI”HIDEKI <chanike1993@yahoo.co.jp>
2021-10-19 17:36:02 +0900

    config.assets.js_compressor = Uglifier.new(harmony: true)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
         provider:              'AWS',
-        aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+        aws_access_key_id:     ENV["AKIAWC22E6XSUCAEV45K"],
+        aws_secret_access_key: ENV["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"],
         region:                'ap-northeast-1',
         path_style:            true,
     }
