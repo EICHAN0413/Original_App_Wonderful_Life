827c1365fb959e0d8311cab922d41cfb8295cebc
”HIDEKIHIDEKI”HIDEKI <chanike1993@yahoo.co.jp>
2021-10-20 18:08:30 +0900

    init

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '13.208.95.226', user: 'app', roles: %w{app db web}
+server '15.152.87.196', user: 'EICHAN', roles: %w{EICHAN db web}
 set :ssh_options, keys: '/Users/hidekiikeda/.ssh/id_rsa'
