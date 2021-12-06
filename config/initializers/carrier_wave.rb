8f6ac62a5931a259119a29f852586fac5f6e79be
”HIDEKIHIDEKI”HIDEKI <chanike1993@yahoo.co.jp>
2021-12-06 18:07:44 +0900

    エラー修正

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,20 +1,22 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
 if Rails.env.production?
     CarrierWave.configure do |config|
       config.storage :fog
-      config.fog_provider = 'fog/aws'  #=> 追加
-      config.fog_directory     =  ENV['S3_BUCKET']
+      config.fog_provider = 'fog/aws' 
       config.fog_credentials = {
         # Amazon S3用の設定
         provider: 'AWS',
         region: ENV['S3_REGION'],     # 例（東京）: 'ap-northeast-1'
         aws_access_key_id: ENV['S3_ACCESS_KEY'],
         aws_secret_access_key: ENV['S3_SECRET_KEY'],
-        path_style: true
       }
-      CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
-      # キャッシュの保存期間
+      config.fog_directory     =  ENV['S3_BUCKET']
+      config.fog_attributes = { 'cache-control' => "max-age=#{365.day.to_i}" }
     end
+
     # 日本語ファイル名の設定
     CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
   end
